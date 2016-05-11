686cac3a00948fe8a3de5e1a3008aa9842e29ffe
eduardoarnold <eduardo.arnold@outlook.com>
2016-05-11 10:13:21 -0300

    https calls into a promise

--- src/cli.js
+++ src/cli.js
@@ -2,14 +2,20 @@
 'use strict';
 
 const program = require('commander');
-
 const resolver = require('gisthrower-resolver');
 
 program
   .version('0.0.1');
 
 program
-  .command('authenticate <username> <token>', 'Persist github personal token locally')
+  .command('auth <username> <token>', 'Persist github personal token locally')
   .action(resolver.authenticate);
+
+program
+  .command('list')
+  .alias('ls')
+  .description('List all gists')
+  .option("-a, --all", "All")
+  .action(resolver.list);
 
 program.parse(process.argv);
