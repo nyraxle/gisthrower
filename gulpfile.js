9ca6273ecfd76e37592d50b9ce1fa7f750cd54a9
eduardoarnold <eduardo.arnold@outlook.com>
2016-05-11 14:23:34 -0300

    remove test files

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,12 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const chalk = require('chalk');
-const nodemon = require('gulp-nodemon');
 const plumber = require('gulp-plumber');
 const eslint = require('gulp-eslint');
-const mocha = require('gulp-mocha');
 
 // Source Paths
 const paths = {
   src: './src',
-  tests: './test/**/*.js'
 };
 paths.srcJS = paths.src + '/**/*.js';
 
@@ -36,28 +33,4 @@ gulp.task('lint', (callback) => {
       gutil.log(fileLog + messageLog + warningLog + errorLog);
     }))
     .pipe(eslint.failAfterError());;
-});
-
-// Nodemon
-gulp.task('start', () => {
-  return nodemon({
-    script: 'src/start.js',
-    ext: 'js',
-    ignore: ['gulpfile.js'],
-    tasks: ['lint']
-  })
-  .on('restart', () => {
-    let log = chalk.inverse('Server restarted due to file changes');
-    gutil.log(log);
-  });
-});
-
-// Tests
-gulp.task('test:mocha', () => {
-  return gulp.src(paths.tests, { read: false })
-    .pipe(mocha());
-});
-
-gulp.task('test:watch', () => {
-  gulp.watch(paths.tests, ['test:mocha']);
 });
