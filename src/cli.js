81e4c64080cc2ec3ddc6d9e5119963a3381feb42
eduardoarnold <eduardo.arnold@outlook.com>
2016-05-11 08:33:37 -0300

    local authentication (to persist token)

--- src/cli.js
+++ src/cli.js
@@ -2,45 +2,14 @@
 'use strict';
 
 const program = require('commander');
-const http = require('https');
 
-const credentials = require('gisthrower-auth').credentials;
+const params = require('gisthrower-params');
+const args = params.args;
+const resolver = params.resolver;
 
 program
   .version('0.0.1')
-  .command('authenticate')
-  .action(function (username, token) {
-    doTest();
-  });
-
-const doTest = () => {
-  let user = credentials.getCredentials();
-
-  const options = {
-    host: 'api.github.com',
-    path: '/gists',
-    method: 'GET',
-    headers: {
-      'User-Agent': 'Ok'
-    },
-    auth: user.username+':'+user.token
-  };
-
-  var req = http.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
-    });
-    // write data to request body
-  });
-
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-  });
-
-  req.end();
-};
+  .command(args.command.AUTHENTICATION)
+  .action(resolver.authenticate);
 
 program.parse(process.argv);
