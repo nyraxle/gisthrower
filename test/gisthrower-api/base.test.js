e209b865c4c217b182befe0b07bba78482410e5d
eduardoarnold <eduardo.arnold@outlook.com>
2016-05-12 15:03:37 -0300

    coverage for api base call

---
+++ test/gisthrower-api/base.test.js
@@ -0,0 +1,37 @@
+'use strict';
+const chai = require('chai');
+const expect = chai.expect;
+
+const appRoot = require('app-root-path');
+const base = appRoot.require('src/node_modules/gisthrower-api/base.js');;
+
+const fs = require('fs');
+const testUserName = process.env.TEST_USER;
+const testToken = process.env.TEST_TOKEN;
+
+describe('Base', () => {
+  before(function() {
+    const filePath = appRoot.path + '\\auth.json';
+    const data = {
+      username: testUserName,
+      token: testToken
+    };
+    const dataJson = JSON.stringify(data);
+    fs.writeFileSync(filePath, dataJson);
+  });
+
+  after(function() {
+    const filePath = appRoot.path + '\\auth.json';
+    fs.unlinkSync(filePath);
+  });
+
+  describe('get', () => {
+    it('should respond with api options', (done) => {
+      base.get('/')
+        .then((result) => {
+          expect(result).to.be.an('object');
+          done();
+        });
+    });
+  });
+});
