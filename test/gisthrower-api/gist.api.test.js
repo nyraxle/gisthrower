3ec5988dd84690c464f3f4eafbcda2a25f56e1ab
Dudu <eduardo.arnold@outlook.com>
2016-10-04 22:49:13 -0300

    fix(clear): remove console log line

--- test/gisthrower-api/gist.api.test.js
+++ test/gisthrower-api/gist.api.test.js
@@ -14,8 +14,6 @@ const fs = require('fs');
 const testUserName = process.env.TEST_USER;
 const testToken = process.env.TEST_TOKEN;
 
-console.log(process.env);
-
 describe('Gist API', () => {
   before(function() {
     const filePath = appRoot.path + '\\auth.json';
